d8ae7e340a607343f9053e468e2cc6c37bd1695b
Jean Dresbach <jeanr.dresbach@gmail.com>
2023-10-19 20:24:16 -0300

    first commit

---
+++ src/assets/js/data.js
@@ -0,0 +1,21 @@
+const cards = [
+    {
+        image: "",
+        title: "Growcast #00",
+        link: "",
+        category: "growcast"
+    }
+]
+
+
+// copiar a tumb  
+// https://img.youtube.com/vi/idDoVideo/sddefault.jpg
+
+
+// copiar url do video
+// 1- abrir video no site da growflix
+// 2- botao direito e clicar em copiar codigo de incorporação
+// vai gerar esse iframe abaixo
+{/* <iframe width="1879" height="805" src="https://www.youtube.com/embed/aJ-HZuLcKeA" title="Growcast #00 | Conheça a Growdev" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe> */}
+// 5- no entro o uri do youtube e o id do video colocar embed como abaixo
+// 6 - https://www.youtube.com/embed/idDoVideo
