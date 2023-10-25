7c906ba1572ea2029257abee526fa688843a2f5c
Jean Dresbach <jeanr.dresbach@gmail.com>
2023-10-24 22:07:43 -0300

    Data of all videos acquired.

--- src/assets/js/data.js
+++ src/assets/js/data.js
@@ -1,21 +1,152 @@
-const cards = [
+const episodes = [
     {
-        image: "",
-        title: "Growcast #00",
-        link: "",
-        category: "growcast"
+        link: "https://www.youtube.com/embed/aJ-HZuLcKeA",
+        title: "Growcast #00 | Conheça a Growdev",
+        image: "https://img.youtube.com/vi/aJ-HZuLcKeA/sddefault.jpg",
+        category: "Growcast [Episódios]"
+    },
+    {
+        link: "https://www.youtube.com/embed/7OWT3lfHYvE",
+        title: "Growcast #01 | As profissões do futuro",
+        image: "https://img.youtube.com/vi/7OWT3lfHYvE/sddefault.jpg",
+        category: "Growcast [Episódios]"
+    },
+    {
+        link: "https://www.youtube.com/embed/ci1eQGPK89o",
+        title: "Growcast #02 | Como seguir a carreira internacional?",
+        image: "https://img.youtube.com/vi/ci1eQGPK89o/sddefault.jpg",
+        category: "Growcast [Episódios]"
+    },
+    {
+        link: "https://www.youtube.com/embed/WM7pGG7OicQ",
+        title: "Growcast #03 | Superando os desafios da carreira de Dev!",
+        image: "https://img.youtube.com/vi/WM7pGG7OicQ/sddefault.jpg",
+        category: "Growcast [Episódios]"
+    },
+    {
+        link: "https://www.youtube.com/embed/2c8VOBZ160Y",
+        title: "Growcast #04 | Agile Master: tudo sobre a profissão!",
+        image: "https://img.youtube.com/vi/2c8VOBZ160Y/sddefault.jpg",
+        category: "Growcast [Episódios]"
+    },
+    {
+        link: "https://www.youtube.com/embed/mxnEm03cYPc",
+        title: "Growcast #5 | Como iniciar na carreira em Desenvolvimento de Softwares?",
+        image: "https://img.youtube.com/vi/mxnEm03cYPc/sddefault.jpg",
+        category: "Growcast [Episódios]"
+    },
+    {
+        link: "https://www.youtube.com/embed/m7DyR07KrOE",
+        title: "Growcast #06 | Mulheres e inclusão na tecnologia",
+        image: "https://img.youtube.com/vi/m7DyR07KrOE/sddefault.jpg",
+        category: "Growcast [Episódios]"
+    },
+    {
+        link: "ttps://www.youtube.com/embed/vW8G9KPwRL0",
+        title: "Growcast #07 | Desenvolvendo profissionais na área de TI",
+        image: "https://img.youtube.com/vi/vW8G9KPwRL0/sddefault.jpg",
+        category: "Growcast [Episódios]"
+    },
+    {
+        link: "https://www.youtube.com/embed/_Ut2KqAqPyk",
+        title: "Growcast #8 | A solução que está transformando o mercado de TI no Brasil",
+        image: "https://img.youtube.com/vi/_Ut2KqAqPyk/sddefault.jpg",
+        category: "Growcast [Episódios]"
+    },
+    {
+        link: "https://www.youtube.com/embed/dfDgs0Ive2Q",
+        title: "Growcast #9 | Acompanhe com quem realmente entende, tudo sobre o mercado de trabalho no mundo da TI.",
+        image: "https://img.youtube.com/vi/dfDgs0Ive2Q/sddefault.jpg",
+        category: "Growcast [Episódios]"
+    },
+    {
+        link: "https://www.youtube.com/embed/WKbLXuP9S-c",
+        title: "Webinar - Recriando a interface do Linkedin com Flutter",
+        image: "https://img.youtube.com/vi/WKbLXuP9S-c/sddefault.jpg",
+        category: "Webinar em Flutter"
+    },
+    {
+        link: "https://www.youtube.com/embed/dJ6y1zMjVXU",
+        title: "Webinar - Recriando a interface do Instagram com Flutter",
+        image: "https://img.youtube.com/vi/dJ6y1zMjVXU/sddefault.jpg",
+        category: "Webinar em Flutter"
+    },
+    {
+        link: "https://www.youtube.com/embed/8rdJsqUjsGg",
+        title: "Webinar - Recriando a interface do iFood com Flutter",
+        image: "https://img.youtube.com/vi/8rdJsqUjsGg/sddefault.jpg",
+        category: "Webinar em Flutter"
+    },
+    {
+        link: "https://www.youtube.com/embed/C4p_7qBsECw",
+        title: "Webinar - Recriando a interface do PicPay com Flutter",
+        image: "https://img.youtube.com/vi/C4p_7qBsECw/sddefault.jpg",
+        category: "Webinar em Flutter"
+    },
+    {
+        link: "https://www.youtube.com/embed/L1pKoMC6hWk",
+        title: "Webinar - Recriando o layout do WhatsApp com Flutter",
+        image: "https://img.youtube.com/vi/L1pKoMC6hWk/sddefault.jpg",
+        category: "Webinar em Flutter"
+    },
+    {
+        link: "https://www.youtube.com/embed/-26aqPtgoEY",
+        title: "Live #1 Jornada UX/UI: O papel do UX/UI Designer no desenvolvimento de um produto digital - Growdev",
+        image: "https://img.youtube.com/vi/-26aqPtgoEY/sddefault.jpg",
+        category: "Jornada UX/UI"
+    },
+    {
+        link: "https://www.youtube.com/embed/8sd3wuKTZMA",
+        title: "Live #2 Jornada UX/UI: O profissional de UX/UI Design no mercado digital - Growdev",
+        image: "https://img.youtube.com/vi/8sd3wuKTZMA/sddefault.jpg",
+        category: "Jornada UX/UI"
+    },
+    {
+        link: "https://www.youtube.com/embed/ZMZjQ9aRuIY",
+        title: "Live #3 Jornada UX/UI: As oportunidades de estudar um programa de formação pagando após formado",
+        image: "https://img.youtube.com/vi/ZMZjQ9aRuIY/sddefault.jpg",
+        category: "Jornada UX/UI"
+    },
+    {
+        link: "https://www.youtube.com/embed/cwqDu0ZzbYE",
+        title: "Live #4 Jornada UX/UI: Como migrar para o UX/UI Design: a trajetória de uma designer - Growdev",
+        image: "https://img.youtube.com/vi/cwqDu0ZzbYE/sddefault.jpg",
+        category: "Jornada UX/UI"
+    },
+    {
+        link: "https://www.youtube.com/embed/FiZMowbaiJA",
+        title: "Live #5 Jornada UX/UI: O UX/UI Design é para todos?",
+        image: "https://img.youtube.com/vi/idDoVideo/sddefault.jpg",
+        category: "Jornada UX/UI"
+    },
+    {
+        link: "https://www.youtube.com/embed/QsuhYgsSYQo",
+        title: "Célula de talentos - Uma solução Growdev",
+        image: "https://img.youtube.com/vi/QsuhYgsSYQo/sddefault.jpg",
+        category: "Diversos"
+    },
+    {
+        link: "https://www.youtube.com/embed/ouDn8z3PSqs",
+        title: "Como é o dia a dia de um dev? Live Growdev com Marcelo Eltz",
+        image: "https://img.youtube.com/vi/ouDn8z3PSqs/sddefault.jpg",
+        category: "Diversos"
+    },
+    {
+        link: "https://www.youtube.com/embed/cQDQNX6NXYo",
+        title: "Onde trabalhar com qualidade de software? Conheça a Testing Company, empresa parceira da Growdev",
+        image: "https://img.youtube.com/vi/cQDQNX6NXYo/sddefault.jpg",
+        category: "Diversos"
+    },
+    {
+        link: "https://www.youtube.com/embed/nKGbolkirBM",
+        title: "WEBINAR ANALISTA DE DADOS - com Paulo Marcon da Arezzo&Co",
+        image: "https://img.youtube.com/vi/nKGbolkirBM/sddefault.jpg",
+        category: "Diversos"
+    },
+    {
+        link: "https://www.youtube.com/embed/6sZPsqNjeek",
+        title: "Tecnologia trazendo soluções para a Arezzo &amp; CO  |  Ricardo Pinho",
+        image: "https://img.youtube.com/vi/6sZPsqNjeek/sddefault.jpg",
+        category: "Diversos"
     }
 ]
-
-
-// copiar a tumb  
-// https://img.youtube.com/vi/idDoVideo/sddefault.jpg
-
-
-// copiar url do video
-// 1- abrir video no site da growflix
-// 2- botao direito e clicar em copiar codigo de incorporação
-// vai gerar esse iframe abaixo
-{/* <iframe width="1879" height="805" src="https://www.youtube.com/embed/aJ-HZuLcKeA" title="Growcast #00 | Conheça a Growdev" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe> */}
-// 5- no entro o uri do youtube e o id do video colocar embed como abaixo
-// 6 - https://www.youtube.com/embed/idDoVideo
